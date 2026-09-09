efdc257d5c222f57b1b03ec49a6b65d02d59f4b0
Nada W. Abdelfattah <59118007+nabdelfattah@users.noreply.github.com>
2026-09-09 13:51:17 +0300

    fix logo aspect ration bug

--- src/foundation/layout/AppSidebar.tsx
+++ src/foundation/layout/AppSidebar.tsx
@@ -257,15 +257,17 @@ const AppSidebar: React.FC = () => {
                 className="dark:hidden"
                 src="/images/logo/logo.svg"
                 alt="Logo"
-                width={150}
-                height={40}
+                width={154}
+                height={32}
+                priority
               />
               <Image
                 className="hidden dark:block"
                 src="/images/logo/logo-dark.svg"
                 alt="Logo"
-                width={150}
-                height={40}
+                width={154}
+                height={32}
+                priority
               />
             </>
           ) : (
